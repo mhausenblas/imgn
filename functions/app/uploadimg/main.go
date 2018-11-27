95f8bc29d2de40bd795328e149d05413a51f24df
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-11-27 15:15:37 +0000

    sets policy for uploader func

--- functions/app/uploadimg/main.go
+++ functions/app/uploadimg/main.go
@@ -6,9 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
@@ -18,8 +22,22 @@ type UploadResponse struct {
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	imgf := strings.NewReader(request.Body)
+	imgfname := "test.png"
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+	uploader := s3manager.NewUploader(sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("imgn-gallery"),
+		Key:    aws.String(imgfname),
+		Body:   imgf,
+	})
+	if err != nil {
+		return serverError(err)
+	}
 	ur := &UploadResponse{
-		Status: "got file",
+		Status: "Successfully uploaded image",
 	}
 	js, err := json.Marshal(ur)
 	if err != nil {
