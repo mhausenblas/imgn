2281069e983c7f901d1f836451a2dbf79c31578b
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-11-27 14:06:14 +0000

    inits list imgs func

--- functions/app/listimgs/main.go
+++ functions/app/listimgs/main.go
@@ -9,19 +9,40 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
 
-type ListResponse struct {
-	Status string `json:"status"`
-}
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	lr := &ListResponse{
-		Status: "the image list",
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+	s3client := s3.New(sess)
+	resp, err := s3client.ListObjects(&s3.ListObjectsInput{Bucket: aws.String("imgn-gallery")})
+	if err != nil {
+		return serverError(err)
 	}
-	js, err := json.Marshal(lr)
+	type Entry struct {
+		Source string `json:"src"`
+		Meta   string `json:"meta"`
+	}
+	flist := []Entry{}
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name:         ", *item.Key)
+		fmt.Println("Last modified:", *item.LastModified)
+		fmt.Println("Size:         ", *item.Size)
+		fmt.Println("Storage class:", *item.StorageClass)
+		fmt.Println("")
+		flist = append(flist, Entry{
+			Source: "/" + *item.Key,
+			Meta:   "meta",
+		})
+	}
+	js, err := json.Marshal(flist)
 	if err != nil {
 		return serverError(err)
 	}
