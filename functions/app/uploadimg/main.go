52d188876c322cb6b1b63eeea9b67b29669ace29
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-11-29 10:53:32 +0000

    changes upload  func to manually parsing form data

--- functions/app/uploadimg/main.go
+++ functions/app/uploadimg/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"mime"
 	"mime/multipart"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -25,52 +27,47 @@ func serverError(err error) (events.APIGatewayProxyResponse, error) {
 	}, nil
 }
 
-func parseFormData(request events.APIGatewayProxyRequest) (imgname, imgct string, imgfile multipart.File, err error) {
-	referrer := request.Headers["Referer"]
-	// create a stdlib HTTP request so that we can use the FormFile methods:
-	r, err := http.NewRequest(request.HTTPMethod, referrer, strings.NewReader(request.Body))
+func parseForm(key, mpheader string, body io.Reader) (string, string, io.Reader, error) {
+	var buf []byte
+	filename := "unknown"
+	contentType := "*"
+	mediaType, params, err := mime.ParseMediaType(mpheader)
 	if err != nil {
 		return "", "", nil, err
 	}
-	fmt.Printf("DEBUG:: HTTP headers: %v\n", request.Headers)
-	// need to set this header in order for FormFile to work
-	// note that the raw multipart header is in this format:
-	// content-type:multipart/form-data; boundary=----WebKitFormBoundaryHW2069S2hMazyq4B
-	mpct := request.Headers["content-type"]
-	r.Header.Set("Content-Type", mpct)
-	r.Header.Set("Content-Length", strconv.Itoa(len(request.Body)))
-	fmt.Printf("DEBUG:: content type: %v\n", mpct)
-	// now let's parse the multipart form data (the key/name used in upload.html is 'file'):
-	mpfile, mpheader, err := r.FormFile("file")
-	if err != nil {
-		return "", "", nil, err
+	if strings.HasPrefix(mediaType, "multipart/") {
+		mr := multipart.NewReader(body, params["boundary"])
+		fmt.Printf("DEBUG:: boundary: %v\n", params["boundary"])
+		for {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				fmt.Printf("DEBUG:: parse EOF\n")
+				break
+			}
+			if err != nil {
+				return "", "", nil, err
+			}
+			buf, err = ioutil.ReadAll(p)
+			if err != nil {
+				return "", "", nil, err
+			}
+			fmt.Printf("DEBUG:: part: %v\n", p.Header)
+			filename = p.FileName()
+			contentType = p.Header.Get("Content-Type")
+		}
 	}
-	defer mpfile.Close()
-	// the name of the image user selected for upload:
-	imgname = mpheader.Filename
-	fmt.Printf("DEBUG:: filename: %v\n", imgname)
-	// the image data itself:
-	imgfile = mpfile
-	// the content type of the image (yeah, not a good practice, but ...)
-	switch {
-	case strings.HasSuffix(imgname, "jpg"), strings.HasSuffix(imgname, "jpeg"):
-		imgct = "image/jpeg"
-	case strings.HasSuffix(imgname, "png"):
-		imgct = "image/png"
-	case strings.HasSuffix(imgname, "txt"):
-		imgct = "text/plain"
-	default:
-		imgct = ""
-	}
-	fmt.Printf("DEBUG:: content type: %v\n", imgct)
-	return imgname, imgct, imgfile, nil
+	fmt.Printf("DEBUG:: parsed file name: %v\n", filename)
+	fmt.Printf("DEBUG:: parsed content type: %v\n", contentType)
+	fmt.Printf("DEBUG:: parsed size in bytes: %v\n", len(buf))
+	// fmt.Printf("DEBUG:: parsed content: %v\n", string(buf))
+	return filename, contentType, strings.NewReader(string(buf)), nil
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Printf("DEBUG:: IsBase64Encoded: %v\n", request.IsBase64Encoded)
 	gallerybucket := "imgn-gallery"
 	// parse the image file name and the data from multipart formdata request:
-	imgname, imgct, imgfile, err := parseFormData(request)
+	imgname, imgct, imgfile, err := parseForm("file", request.Headers["content-type"], strings.NewReader(request.Body))
 	if err != nil {
 		return serverError(err)
 	}
@@ -89,8 +86,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		Bucket:      aws.String(gallerybucket),
 		Key:         aws.String(imgname),
 		ContentType: aws.String(imgct),
-		// ContentEncoding: aws.String("base64"),
-		Body: imgfile,
+		Body:        imgfile,
 	})
 	if err != nil {
 		return serverError(err)
